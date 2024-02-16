eea83a2a57f4c54b35bf51b4434df3518fed54df
viktorcessan <viktor@cessan.se>
2024-02-16 16:40:39 +0100

    Made array global method. Accept user input Calculate user input. Print array if user input is correct.

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,12 @@ import java.util.Scanner;
 public class Main {
     public static void welcomeMessage () {
         System.out.println("Hello and welcome to Viktors Sudoku");
-        System.out.println("Guess the missing number");
+        System.out.println("Guess the missing number, what should replace the question mark?");
     }
-    public static void main(String[] args) {
-        Scanner userInput=new Scanner(System.in);
-
-        int[][] sudokuNumbersarray = {{1,2,3},{1,2,3},{1,2,3}};
-        welcomeMessage();
+    public static void guessNumber(){
+        System.out.println("What number do you think it is?");
+    }
+    public static void printArray(String[][]sudokuNumbersarray){
         System.out.println("_____________");
         for (int i=0; i<sudokuNumbersarray.length;i++) {
             System.out.print("| ");
@@ -18,9 +17,23 @@ public class Main {
             System.out.println();
         }
         System.out.println("_____________");
+    }
+    public static void main(String[] args) {
+        Scanner userInput=new Scanner(System.in);
+
+        String[][] sudokuNumbersarray = {{"1","2","3"},{"2","3","1"},{"3","1","?"}};
+        welcomeMessage();
+        printArray(sudokuNumbersarray);
       int[] sudokuInputarray = {1};
       sudokuInputarray[0] = userInput.nextInt();
-
+      guessNumber();
+      if (Integer.parseInt(sudokuNumbersarray[2][0])+Integer.parseInt(sudokuNumbersarray[2][1])+sudokuInputarray[0]==6){
+          sudokuNumbersarray[2][2]=Integer.toString(sudokuInputarray[0]);
+          System.out.println("Awesome, you were correct and win the glorious competition.");
+          printArray(sudokuNumbersarray);
+          }
+      else {System.out.println("Wrong answer");
+      }
     }
 }
-//todo add randomizer, hide number, add calculation logic
+//todo add randomizer,
